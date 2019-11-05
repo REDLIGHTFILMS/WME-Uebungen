ec29b2870a7091bbde87c7ab8a28e84612f12b9a
Albert <letsplaygermantv1@googlemail.com>
2019-11-05 10:00:58 +0100

    test

--- index.js
+++ index.js
@@ -1,5 +1,47 @@
-function test() {
+document.getElementById("button").onclick = function test() {
   $.ajax({
-    url: "./"
+    type: "GET",
+    url: "./source/world_data_v1.csv",
+    dataType: "text",
+    success: function(response) {
+      data = $.csv.toArrays(response);
+      generateHtmlTable(data);
+    }
   });
+};
+
+function generateHtmlTable(data) {
+  var html = '<table  class="table table-condensed table-hover table-striped">';
+
+  if (typeof data[0] === "undefined") {
+    return null;
+  } else {
+    $.each(data, function(index, row) {
+      //bind header
+      if (index == 0) {
+        html += "<thead>";
+        html += "<tr>";
+        $.each(row, function(index, colData) {
+          html += "<th>";
+          html += colData;
+          html += "</th>";
+        });
+        html += "</tr>";
+        html += "</thead>";
+        html += "<tbody>";
+      } else {
+        html += "<tr>";
+        $.each(row, function(index, colData) {
+          html += "<td>";
+          html += colData;
+          html += "</td>";
+        });
+        html += "</tr>";
+      }
+    });
+    html += "</tbody>";
+    html += "</table>";
+    alert(html);
+    $("#csv-display").append(html);
+  }
 }
