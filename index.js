34b63878c7884e29c709ad02a99f9cfb98ae5a0a
REDLIGHTFILMS <33683591+REDLIGHTFILMS@users.noreply.github.com>
2019-11-10 16:29:02 +0100

    switcher is complete

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
-function showhide(value) {
-  $(`th:nth-child(${value})`).hide();
-  $(`td:nth-child(${value})`).hide();
+function sortTable(n) {
+  var table, rows, switchn, i, x, y, shouldSwitching;
+  table = document.getElementById("ourtable");
+  switchn = true;
+
+  while (switchn) {
+  
+    switchn = false;
+    rows = table.rows;
+
+    for (i = 1; i < (rows.length - 1); i++) {
+    
+      shouldSwitching = false;
+
+      if(n == 1){
+        x = rows[i].getElementsByTagName("TD")[0];
+        y = rows[i + 1].getElementsByTagName("TD")[0];
+      }else{
+        x = rows[i + 1].getElementsByTagName("TD")[0];
+        y = rows[i].getElementsByTagName("TD")[0];
+      }
+
+
+      if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+        shouldSwitching = true;
+        break;
+      }
+    }
+    if (shouldSwitching) {
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switchn = true;
+    }
+  }
 }
