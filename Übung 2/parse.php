aeca4e086746b7266238c9302e1e55bb6bb898d1
REDLIGHTFILMS <33683591+REDLIGHTFILMS@users.noreply.github.com>
2019-11-30 13:24:11 +0100

    parse.php

--- Übung 2/parse.php
+++ Übung 2/parse.php
@@ -1,7 +1,20 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
 <?php
-function parseCSV()
-{
-    $file = fopen("world_data.csv", "r");
-    print_r(fgetcsv($file));
-    fclose($file);
-}
+    function parseCSV()
+    {
+         $file = file("world_data.csv");
+        foreach($file as $k)
+            $csv[] = str_getcsv($k);
+        print_r($csv);
+    }
+?>
+</body>
+</html>
+
