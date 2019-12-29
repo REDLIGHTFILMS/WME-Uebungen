339ed0bd278922512e4f7860907327dc795393e1
REDLIGHTFILMS <33683591+REDLIGHTFILMS@users.noreply.github.com>
2019-12-29 13:57:39 +0100

    add csvtojson function and try to add get requests

--- Übung 3/Team_00/app.js
+++ Übung 3/Team_00/app.js
@@ -1,28 +1,38 @@
 // DO NOT CHANGE!
 //init app with express, util, body-parser, csv2json
 var express = require('express');
+var router = express.Router();
 var app = express();
 var sys = require('util');
 var path = require('path');
 var bodyParser = require('body-parser');
-var Converter = require("csvtojson").Converter;
+var Converter = require("csvtojson");
+var csvFilePath = require("./world_data.csv");
 
 //register body-parser to handle json from res / req
-app.use( bodyParser.json() );
+app.use( bodyParser.json());
 
 //register public dir to serve static files (html, css, js)
 app.use( express.static( path.join(__dirname, "public") ) );
 
 // END DO NOT CHANGE!
-
-
 /**************************************************************************
 ****************************** csv2json *********************************
 **************************************************************************/
 
+var json;
+
+Converter().fromFile("./world_data.csv").then(source =>{
+   source = json;
+})
+
+router.get("/items")
+
+
 /**************************************************************************
 ********************** handle HTTP METHODS ***********************
 **************************************************************************/
+
 
 
 // DO NOT CHANGE!
