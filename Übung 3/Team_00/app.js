57a706d54e5d86364a70ede1e93442d59410e16b
Tobias Scholz <tobiasscholz5@web.de>
2020-01-06 07:55:03 +0100

    did some work

--- Übung 3/Team_00/app.js
+++ Übung 3/Team_00/app.js
@@ -1,76 +1,145 @@
 // DO NOT CHANGE!
 //init app with express, util, body-parser, csv2json
 var express = require('express');
-var router = express.Router();
 var app = express();
 var sys = require('util');
 var path = require('path');
 var bodyParser = require('body-parser');
-var Converter = require("csvtojson");
-var csvFilePath = require("./world_data.csv");
+var Converter = require("csvtojson").Converter;
 
 //register body-parser to handle json from res / req
-app.use( bodyParser.json());
+app.use( bodyParser.json() );
 
 //register public dir to serve static files (html, css, js)
 app.use( express.static( path.join(__dirname, "public") ) );
 
 // END DO NOT CHANGE!
+
+
 /**************************************************************************
 ****************************** csv2json *********************************
 **************************************************************************/
 
-var json;
+//1. Parsen der CSV und speichern als JSON
+var items = {};
 
-Converter().fromFile("./world_data.csv").then(source =>{
-   json = source;
-})
-
-app.get('/items', (req, res) => {
-    res.json(json)
-})
-
-app.get('/items/:id', (req, res) => {
-
-    const found = json.some(json => json.id === req.params.id);
-
-    if(found){
-        res.json(json.filter(json => json.id === req.params.id));
-    } else{
-        res.status(400).json({msg: `No such id ${req.params.id} in database.`});
-    }
-    
-})
-
-app.get('/items/:id1/:id2', (req, res) => {
-    const found = json.some(json => json.id === req.params.id1);
-
-    if(found){
-        let filtered = json.filter(e => {return e.id >= req.params.id1 && e.id <= req.params.id2});
-        res.json(filtered);
-    } else{
-        res.status(400).json({msg: `No such id ${req.params.id1} in database.`});
-    }
-
-   
-    
-})
-
-app.get('/properties', (req, res) => {
-    
-})
-
-app.get('/properties/:num', (req, res) => {
-    
-})
-
+var Converter = require("csvtojson").Converter;
+var converter = new Converter({});
+converter.fromFile("./world_data.csv", function(err, jsonArray){
+	items = jsonArray;
+});
 
 
 
 /**************************************************************************
 ********************** handle HTTP METHODS ***********************
 **************************************************************************/
+//2. Erstellen der REST-API
+// GET-CALLS#
+//items
+app.get('/items', function(req, res, next){
+	res.send({ items: items});
+});
 
+// items id
+app.get('/items/:id', function(req, res, next){
+	var id = req.params.id;
+	var country = items[id];
+	var found = false;
+	
+	if(typeof items.id !== undefined){
+ 		items.forEach(function(item, i) {
+			if (!found && item.id === Number(id)) {
+				res.send(items[i]);
+				found = true;
+			} 
+		});
+	} 
+	
+	if(!found) {
+		res.json({error : "No such id " + id + " in database"});
+	}
+});
+
+// items id1 id2
+app.get('/items/:id1/:id2', function(req, res, next){
+
+	var id1 = req.params.id1;
+	var id2 = req.params.id2;
+	var countrys = [];
+	var found = false;
+	
+
+	for (var i = id1-1, j = id2-1; i <= j; i++) {
+		countrys.push(items[i]);
+		found = true;
+	}
+
+	res.send(countrys);
+});
+// properties
+app.get('/properties', function(req, res, next){
+	var properties = Object.getOwnPropertyNames(items[1]);
+	res.send(properties);
+});
+
+// properties num
+app.get('/properties/:num', function(req, res, next){
+	var num = req.params.num;
+	var properties = Object.getOwnPropertyNames(items[1]);
+	var property = properties[num];
+	res.send(property);
+});
+
+// POST-CALLS
+
+app.post('/items', function(req, res, next) {
+	var country_name = req.body.name;
+	var property1 = req.body['birth_rate_per_1000'];
+	var property2 = req.body['cell_phones_per_100'];
+
+	var currentID = Object.keys(items).length+1;
+
+	items.push({
+		id: currentID,
+		name: country_name,
+		['birth_rate_per_1000']: property1,
+		['cell_phones_per_100']: property2
+	});
+	res.send('Added country ' + country_name + '  to list');
+});
+
+// DELETE-CALLS
+//items delete
+app.delete('/items', function(req, res, next) {
+	var currentID = Object.keys(items).length-1;
+	var deleted_name = JSON.stringify(items[currentID].name);
+	
+	items.splice(currentID, 1);
+
+    res.send('Deleted last country: ' + deleted_name);
+});
+//items id delete
+app.delete('/items/:id', function(req, res){
+	var id = req.params.id;
+	var found = false;
+	
+	if(typeof items.id == "undefined"){
+		items.forEach(function(item, i) {
+			if (!found && item.id === Number(id)) {
+				items.splice(i, 1);
+				found = true;
+			}
+		});
+	}
+
+	if(found) {
+		res.send({success : 'Item ' + id + ' deleted successfully.', status : 200});
+	} else {
+		res.json({error : "No such id " + id + " in database"});
+	}
+	
+});
 
 
 // DO NOT CHANGE!
--- Übung 3/Team_00/public/assets/js/ajax.js
+++ Übung 3/Team_00/public/assets/js/ajax.js
@@ -1,1 +1,177 @@
-alert("No ajax calls implemented ;)");
+//3.
+//Tabelle erstellen
+$(document).ready(function() {
+	$.getJSON( "/items", function( data ) {
+		$.each(data.items, function(i, f) {
+			var tblRow = "<tr>" + 
+				"<td>" + f.id + "</td>" + 
+				"<td>" + f.name + "</td>" + 
+				"<td>" + f['birth_rate_per_1000'] + "</td>" + 
+				"<td>" + f['cell_phones_per_100'] + "</td>"  +
+				"<td>" + f['children_per_woman'] + "</td>"  +
+				"<td>" + f['electricity_consumption_per_capita']+ "</td>"  +
+				"<td>" + f['gdp_per_capita'] + "</td>" +
+				"<td>" + f['gdp_per_capita_growth'] + "</td>" +
+				"<td>" + f['inflation_annual'] + "</td>" +
+				"<td>" + f['internet_user_per_100'] + "</td>" + 
+				"<td>" + f['life_expectancy'] + "</td>" +
+				"<td>" + f['military_expenditure_percent_of_gdp'] + "</td>" +
+				"<td>" + f['gps_lat'] + "</td>" +
+				"<td>" + f['gps_long'] + "</td>" +
+				"</tr>";
+		$(tblRow).appendTo("#table_body");
+		});
+	});
+	
+	// Filter Countries
+	// nach country id
+	$('#country_filter').on('submit', function(event) {
+		
+			if($('#country_filter_range').val() == false) {
+				event.preventDefault();
+				var id = $('#country_filter_id').val();
+				$( "#table_body" ).empty();
+				$.getJSON( "/items" + "/" + id, function(f) {
+						$( "#table_body" ).empty();
+						var tblRow = "<tr>" + 
+							"<td>" + f.id + "</td>" + 
+							"<td>" + f.name + "</td>" + 
+							"<td>" + f['birth_rate_per_1000'] + "</td>" + 
+							"<td>" + f['cell_phones_per_100'] + "</td>"  +
+							"<td>" + f['children_per_woman'] + "</td>"  +
+							"<td>" + f['electricity_consumption_per_capita']+ "</td>"  +
+							"<td>" + f['gdp_per_capita'] + "</td>" +
+							"<td>" + f['gdp_per_capita_growth'] + "</td>" +
+							"<td>" + f['inflation_annual'] + "</td>" +
+							"<td>" + f['internet_user_per_100'] + "</td>" + 
+							"<td>" + f['life_expectancy'] + "</td>" +
+							"<td>" + f['military_expenditure_percent_of_gdp'] + "</td>" +
+							"<td>" + f['gps_lat'] + "</td>" +
+							"<td>" + f['gps_long'] + "</td>" +
+							"</tr>";
+						$(tblRow).appendTo("#table_body");
+				});
+			} 
+	});	
+		// filter countries nach country id range
+		$('#country_filter').on('submit', function(event) {
+		if($('#country_filter_range').val()) {
+			event.preventDefault()
+			
+			var range = $('#country_filter_range').val();
+			var res = range.split("-");
+			var id1 = res[0];	
+			var id2 = res[1];
+			
+			$( "#table_body" ).empty();
+			$.getJSON( "/items" + "/" + id1 + "/" + id2, function(data) {
+				$.each(data, function(i,f) {
+					var tblRow = "<tr>" + 
+						"<td>" + f.id + "</td>" + 
+						"<td>" + f.name + "</td>" + 
+						"<td>" + f['birth_rate_per_1000'] + "</td>" + 
+						"<td>" + f['cell_phones_per_100'] + "</td>"  +
+						"<td>" + f['children_per_woman'] + "</td>"  +
+						"<td>" + f['electricity_consumption_per_capita']+ "</td>"  +
+						"<td>" + f['gdp_per_capita'] + "</td>" +
+						"<td>" + f['gdp_per_capita_growth'] + "</td>" +
+						"<td>" + f['inflation_annual'] + "</td>" +
+						"<td>" + f['internet_user_per_100'] + "</td>" + 
+						"<td>" + f['life_expectancy'] + "</td>" +
+						"<td>" + f['military_expenditure_percent_of_gdp'] + "</td>" +
+						"<td>" + f['gps_lat'] + "</td>" +
+						"<td>" + f['gps_long'] + "</td>" +
+						"</tr>";
+				$(tblRow).appendTo("#table_body");
+				});
+			});
+		} else { 
+			if($('#country_filter_id').val() == false ) { 
+			location.reload(); 
+			}	
+		}
+	});
+	
+	//Properties Abfragen
+	$.getJSON( "/properties", function( property ) {
+		$.each(property, function(i) {
+			var options = "<option value=" + i + ">"+ property[i] + "</option>";
+		$(options).appendTo("#prop_selection");	
+		});
+	});
+	//hide
+	$('#hide_selected_prop').click('submit', function(event) {
+		event.preventDefault();
+        var num = $("#prop_selection").val();
+		num++;
+        $.get("/properties/" + num, function(data) {
+			$('td:nth-child('+num+')').hide();
+			$('th:nth-child('+num+')').hide();
+		});
+	});
+	//show
+	$('#show_selected_prop').click('submit', function(event) {
+		event.preventDefault();
+        var num = $("#prop_selection").val();
+		num++;
+        $.get("/properties/" + num, function(data) {
+			$('th:nth-child('+num+')').show();
+			$('td:nth-child('+num+')').show();
+		});
+	});
+	
+	//Add COuntry
+
+    $('#country_add').on('submit', function(event) {
+        event.preventDefault();
+
+        var country_name = $('#country_name');
+		var property1_input = $('#country_birth');
+		var property2_input = $('#country_cellphone');
+
+        $.ajax({
+            url: '/items',
+            method: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({ name: country_name.val(), ["birth_rate_per_1000"]: property1_input.val(), ['cell_phones_per_100']: property2_input.val()}),
+            success: function(response) {
+                console.log(response);
+				location.reload();
+            }
+        });
+    });
+
+	//Delete Country
+	$('#country_delete').on('submit', function(event) {
+		event.preventDefault();
+		//keine id angegeben
+		if($('#country_delete_id').val() == false) {
+			$.ajax({
+				url: '/items',
+				method: 'DELETE',
+				contentType: 'application/json',
+				success: function(response) {
+					console.log(response);
+					location.reload();
+				}
+			});
+		}
+	});
+	
+	$('#country_delete').on('submit', function(event) {
+		event.preventDefault();
+		if($('#country_delete_id').val()) {
+			var id = $('#country_delete_id').val();
+			
+			$.ajax({
+				url: '/items/' + id,
+				method: 'DELETE',
+				contentType: 'application/json',
+				success: function(response) {
+					console.log(response);
+					location.reload();
+				}
+			});
+		}
+	});
+});
